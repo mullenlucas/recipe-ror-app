4dd5c66b955da578120511ca8d9c778b4e0959d9
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2023-01-05 01:03:35 +0430

    update resipes model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: :user_id
+  has_many :recipefoods, foreign_key: :recipe_id, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
+
 end
