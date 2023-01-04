eb277ca3e5d1bd1092978a7ad4032127054bd586
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2023-01-05 00:56:58 +0430

    update model for food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,8 @@
 class Food < ApplicationRecord
-  belongs_to :user
+  belongs_to :user,foreign_key: :user_id
+  has_many :recipefoods, foreign_key: :food_id
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true
 end
