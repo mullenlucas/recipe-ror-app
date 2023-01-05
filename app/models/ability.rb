79a3cd7628f58c3fa7d13787210445637a868a2f
mullenlucas <mullenlucas@gmail.com>
2023-01-05 17:28:39 -0300

    omit hash rubocop offense fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,10 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    can :read, Recipe, user: user
-    can :manage, Recipe, user: user
-    can :manage, Food, user: user
-    can :manage, RecipeFood, user: user
+    can :read, Recipe, user:
+    can :manage, Recipe, user:
+    can :manage, Food, user:
+    can :manage, RecipeFood, user:
     return unless user.role == 'admin'
 
     can :manage, :all
