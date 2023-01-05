d3b2dfc6815b008ca44a96113755cc0763dc0042
mullenlucas <mullenlucas@gmail.com>
2023-01-05 17:34:51 -0300

    fix rubocop offense

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,10 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    can :read, Recipe, user:
-    can :manage, Recipe, user:
-    can :manage, Food, user:
-    can :manage, RecipeFood, user:
+    can :read, Recipe, user
+    can :manage, Recipe, user
+    can :manage, Food, user
+    can :manage, RecipeFood, user
     return unless user.role == 'admin'
 
     can :manage, :all
