2117b536339abd72dbb1def6cc533512bf249954
mullenlucas <mullenlucas@gmail.com>
2023-01-07 15:18:52 -0300

    slight fix in recipes spec request

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -8,10 +8,14 @@ RSpec.describe 'PublicRecipe', type: :feature do
     end
 
     feature 'Testing post index page' do
-      background { visit public_recipes_index_path }
+      background { visit public_recipes_path }
 
       scenario 'I can see the title' do
         expect(page).to have_content('Recipe')
+      end
+
+      scenario 'I can see the app title' do
+        expect(page).to have_content('Candent')
       end
 
       scenario 'I can see the sign out button' do
