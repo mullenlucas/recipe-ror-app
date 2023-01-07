d731a08159b893b7a02c4174589c8f9664f39c25
mullenlucas <mullenlucas@gmail.com>
2023-01-07 12:25:18 -0300

    update recipe and user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new(name: 'Nuk', email: 'nuk@email.com', password: 'lalala')
+    @user.save
+  end
+
+  it 'is created succesfully' do
+    expect(@user).to be_valid
+  end
+
+  it 'name is not necessarily required' do
+    expect(@user).to be_valid
+    @user.name = nil
+    expect(@user).to be_valid
+  end
+
+  it 'is not valid without email' do
+    @user.email = nil
+    expect(@user).not_to be_valid
+  end
+
+  it 'is not valid without password' do
+    @user.password = ''
+    expect(@user).not_to be_valid
+  end
 end
