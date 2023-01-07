e1885d15c7b20b7951ebacb25b4f49f8bacf98d6
tamana Balkhi <tamana.balkhi1377@gmail.com>
2023-01-07 22:31:45 +0430

    Revert "add Styles "

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :shopping_lists
   resources :shopping_lists
-  get 'recipes/index'
+  get 'public_recipes/index'
   devise_for :users
 
-  root 'recipes#index'
+  root 'public_recipes#index'
   resources :users do
     resources :foods, only: %i[index new create]
     resources :recipes, only: %i[index show new create destroy] do
